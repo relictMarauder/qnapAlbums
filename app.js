bd5ad0ae9f19f7e9ad46e43eabdf798dae5a868a
Alexander Bonelis <alexander@bonelis.de>
2015-08-23 14:23:02 +0200

    add deployment config fix for not existed folders fix mount point

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var fs = Promise.promisifyAll(require("fs"));
 var path = require('path');
 var moment =require('moment');
 
-var mountPoint = "/shares/CACHEDEV1_DATA/";
+var mountPoint = "/share/CACHEDEV1_DATA/";
 qnapAlbumsMgmr.version('0.0.1')
     .usage('<option> [parameter]')
     .option("-e, --export [albumName]", "export picture albums to file")
     .option("-i, --import [fileName]", "import picture albums from file")
     .option("-l, --list [albumName]", "list of albums/content of album if album name is defined")
-    .option("-s, --save <path>", "export picture albums as Folders with softlinks on piktures")
+    .option("-s, --save <path>", "export picture albums as Folders with softlinks on pictures")
     .parse(process.argv);
 
 
@@ -68,10 +68,12 @@ function synchronizeWithFolder(syncPath) {
                     var exportFolder = path.join(syncPath + albumRow.cAlbumTitle.replace("[/\\:'\"]", "_"),"/");
                     return fs
                         .statAsync(exportFolder)
-                        .catch(function(){})
                         .then(function () {
                             //console.log("delete existed folder "+exportFolder);
                             return rmdir(exportFolder);
+                        })
+                        .catch(function(){
+                            //console.log("folder "+exportFolder+" don't exist")
                         })
                         .then(function(){
                             //console.log("create folder "+exportFolder);
@@ -255,134 +257,3 @@ function AlbumNotFoundError(message, extra) {
 };
 
 require('util').inherits(AlbumNotFoundError, Error);
-
-/*var selectedAlbumName = 'Dubai-2015';
- var selectedAlbumId;
- var conn = Promise.promisifyAll(connection);
- conn.connectAsync()
- /*
- .then(function(){
- return conn.queryAsync('SELECT * FROM pictureAlbumTable').spread(function(albumRows,fields){
- console.log('Finded '+albumRows.length+" albums");
- return albumRows;
- })
- .map(function(albumRow){
- //console.log('The album is: ', JSON.stringify(albumRow, null, 2));
- var queryStr = 'SELECT pt.* FROM pictureTable pt INNER JOIN pictureAlbumMapping ptAlb ON pt.iPictureId=ptAlb.iMediaId where ptAlb.iPhotoAlbumId='+albumRow.iPhotoAlbumId;
- if (albumRow.cAlbumTitle==selectedAlbumName){
- selectedAlbumId=albumRow.iPhotoAlbumId;
- }
- return conn.queryAsync(queryStr)
- .spread( function(pictureRows,fields){
- console.log("Album \""+albumRow.cAlbumTitle + "\" has "+pictureRows.length+" pictures");
-
- });
- });
- })
- .then(function(){
- return findPhotosFronFileInDB("./listofFiles")
- })*/
-/*.then(function () {
- return getListOfPicturesFroAlbum(selectedAlbumName)
- })
- .then(function () {
- process.exit(0);
- console.log("end");
- })
-
- function getListOfPicturesFroAlbum(albumName) {
- return conn.queryAsync('SELECT pt.*, dt.cFullPath FROM pictureTable pt INNER JOIN pictureAlbumMapping ptAlb ON pt.iPictureId=ptAlb.iMediaId INNER JOIN pictureAlbumTable pa ON pa.iPhotoAlbumId=ptAlb.iPhotoAlbumId INNER JOIN dirTable dt ON dt.iDirId=pt.iDirId where pa.cAlbumTitle="' + albumName + '"').spread(function (pictureRows, fields) {
- for (var i = 0, len = pictureRows.length; i < len; i++) {
- console.log("/shares/" + pictureRows[i].cFullPath + pictureRows[i].cFileName)
- }
- })
- }
- function findPhotosFronFileInDB(fileListName) {
- return fs.readFileAsync(fileListName)
- .then(function (str) {
- return str.toString().split('\n');
- })
- .then(function (fileNames) {
- console.log("Start processing of " + fileNames.length + " files");
- return fileNames;
- })
- .map(findPhotoInDB)
- .then(function (pictureIds) {
- console.log("pictureIds:" + pictureIds.length);
- var arrayOfCommands = [];
- for (var i = 0, len = pictureIds.length; i < len; i++) {
- arrayOfCommands.push({
- command: "addToPicAlbum",
- parameter: ["/share/CACHEDEV1_DATA/", selectedAlbumId.toString(), pictureIds[i].toString(), "1"]
- })
- }
- var cmdObj = {mymediadbcmd: arrayOfCommands}
- return fs.writeFileAsync('tmp_cmd.cmd', JSON.stringify(cmdObj, null, 1))
- });
- }
-
- function findPhotoInDB(fileName) {
- fileName = fileName.replace('*', '');
- var queryStr = 'SELECT pt.* FROM pictureTable pt WHERE pt.cFileName=\"' + fileName + '\"';
- //console.log(queryStr)
- return conn.queryAsync(queryStr)
- .spread(function (fileObjs, fields) {
- if (fileObjs.length === 1) {
- //console.log('File '+fileName+' is found, pictureId: '+fileObjs[0].iPictureId)
- return fileObjs[0].iPictureId;
- }
- else {
- console.error('File ' + fileName + ' is not found:' + JSON.stringify(fileObjs, null, 2))
- }
- })
- .catch(function (error) {
- console.error('File ' + fileName + ' is not found, error:' + JSON.stringify(error, null, 2))
- });
- }
-
-
- /*
- function addPhotoToAlbum($photoId, $albumId){^M
- $type = self::MEDIA_TYPE_PHOTO;^M
- if(!is_array($photoId)){^M
- unset($returnArray);^M
- $photoId = escapeshellarg($photoId);^M
- $albumId = escapeshellarg($albumId);^M
- $command = "{$this->CMD} addToPicAlbum {$this->media_root} $albumId $photoId $type";^M
- $this->simpleExec($command, $returnArray, $returnVar);^M
- return array('status'=>$returnVar, 'output'=>$returnArray, 'commands'=>$this->getCommandHistory(), 'Queries'=>$this->getQueryHistory());^M
- }else{^M
- //do batch add photos^M
- $data = array();^M
- foreach($photoId as $photo){^M
- $data[] = array(^M
- "command"=>"addToPicAlbum",^M
- "parameter"=> array(^M
- "{$this->media_root}",^M
- "$albumId",^M
- "$photo",^M
- "$type"^M
- )^M
- );^M
- }^M
- $file = '/tmp/batch_addphoto_'.strtotime('now').'_'.mt_rand(100,999);^M
- file_put_contents($file, qnap_json_encode(array('mymediadbcmd'=>$data)));^M
- $command = "{$this->CMD} cmdFile $file";^M
- $this->simpleExec($command, $returnArray, $returnVar);^M
- return array('status'=>$returnVar, 'output'=>$returnArray, 'commands'=>$this->getCommandHistory(), 'Queries'=>$this->getQueryHistory());^M
- }^M
- }^M
- *
-
- /*connection.query('SELECT * FROM `pictureAlbumTable` LIMIT 100', function(err, albumRows, fields) {
- if (err) throw err;
- console.log('Finded '+albumRows.length+" Albums");
- for (var i = 0, len = albumRows.length; i < len; i++ ){
- var albumObj = albumRows[i];
- connection.query(' SELECT pt.cFileName FROM pictureTable pt INNER JOIN pictureAlbumMapping ptAlb ON pt.iPictureId=ptAlb.iMediaId where ptAlb.iPhotoAlbumId=3', function(err, pictureRows, fields) {
- console.log("Album :"+albumObj.cAlbumTitle + " with "+pictureRows.lenght);
-
- });
- }
- //  console.log('The solution is: ', JSON.stringify(rows, null, 2));
- });*/
